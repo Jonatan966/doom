30f3826e7645be2ee0f287212e84a8852ea5d542
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-23 02:04:11 -0300

    feat: app theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components'
+
+interface ThemeItem {
+  primary: string
+  secondary: string
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      text: ThemeItem
+      box: ThemeItem
+      background: string
+    }
+    effects: {
+      shadow: ThemeItem
+    }
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const AppTheme = {
+  colors: {
+    text: {
+      primary: '#000000',
+      secondary: '#F1FAEE',
+    },
+    box: {
+      primary: '#1D3557',
+      secondary: '#A8DADC',
+    },
+    background: '#457B9D',
+  },
+  effects: {
+    shadow: {
+      primary: '0px 0px 8px rgba(0, 0, 0, 0.25)',
+      secondary: 'inset 0px 0px 8px rgba(0, 0, 0, 0.25)',
+    },
+  },
+}
