4430cafc2cc3e53499e60145832f7dde3f11cea6
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-24 21:21:39 -0300

    feat: implement route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { GlobalStyle } from './styles/GlobalStyle'
+import { ThemeProvider } from 'styled-components'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
-import { Greetings } from './components/Greetings'
+import { SplashPage } from './pages/Splash'
+
+import { GlobalStyle } from './styles/GlobalStyle'
+import { AppTheme } from './styles/theme'
+import { HomePage } from './pages/Home'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={AppTheme}>
       <GlobalStyle />
-      <Greetings />
-    </>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={SplashPage} />
+          <Route exact path="/home" component={HomePage} />
+        </Switch>
+      </HashRouter>
+    </ThemeProvider>
   )
 }
