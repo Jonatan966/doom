cd3482f3fd0dca346ca7c63e577d8abe87f7da89
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-07-27 13:06:18 -0300

    feat: register events

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,7 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
+import { checkResources } from './events/checkResources'
+import { listSounds } from './events/listSounds'
+import { readSound } from './events/loadSound'
 
 let mainWindow: BrowserWindow | null
 
@@ -10,17 +13,18 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 //     ? process.resourcesPath
 //     : app.getAppPath()
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     // icon: path.join(assetsPath, 'assets', 'icon.png'),
+    autoHideMenuBar: true,
     width: 1100,
     height: 700,
     backgroundColor: '#191622',
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
-    }
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
   })
 
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
@@ -30,16 +34,23 @@ function createWindow () {
   })
 }
 
-async function registerListeners () {
-  /**
-   * This comes from bridge integration, check bridge.ts
-   */
-  ipcMain.on('message', (_, message) => {
-    console.log(message)
+async function registerListeners() {
+  ipcMain.on('check-resources', () => {
+    mainWindow?.webContents.send('finish-check-resources', checkResources())
+  })
+
+  ipcMain.on('sound-list', (_, path) => {
+    const soundList = listSounds(path)
+    mainWindow?.webContents.send('retrieve-sound-list', soundList)
+  })
+
+  ipcMain.on('load-sound', (event, soundPath) => {
+    readSound(soundPath).then(url => event.sender.send('loaded-sound', url))
   })
 }
 
-app.on('ready', createWindow)
+app
+  .on('ready', createWindow)
   .whenReady()
   .then(registerListeners)
   .catch(e => console.error(e))
