c1bc1eb8273c72175cce85f80f3711f1d0414229
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-03 17:06:23 -0300

    feat: explorer item card

--- src/components/SoundExplorer/index.tsx
+++ src/components/SoundExplorer/index.tsx
@@ -3,20 +3,53 @@ import { GoTrashcan } from 'react-icons/go'
 
 import { MdMusicNote, MdFolder } from 'react-icons/md'
 import { ExplorerItem } from '../../@types/explorerItem'
-import { usePlayer } from '../../contexts/playerContext'
+import { CurrentSound, usePlayer } from '../../contexts/playerContext'
 
 import { Button } from '../Button'
 import { VerticalList } from '../VerticalList'
-import { Container, Header } from './styles'
+import { Container, ExplorerItemCardContainer, Header } from './styles'
 
 interface SoundExplorerProps {
   ableToPlaySound?: boolean
   ableToRemoveItem?: boolean
+  onSelect?: (currentSound: CurrentSound) => void
+  className?: string
+}
+
+interface ExplorerItemCardProps extends ExplorerItem {
+  onNavigate?: () => void
+  onRemove?: () => void
+  onSelect?: () => void
+}
+
+function ExplorerItemCard({
+  name,
+  type,
+  onNavigate,
+  onSelect,
+  onRemove,
+}: ExplorerItemCardProps) {
+  return (
+    <ExplorerItemCardContainer className={onRemove ? 'editable' : ''}>
+      <Button onClick={type === 'folder' ? onNavigate : onSelect}>
+        {type === 'sound' && <MdMusicNote size={25} />}
+        {type === 'folder' && <MdFolder size={25} />}
+        <h2>{name}</h2>
+      </Button>
+      {onRemove && (
+        <Button className="delete-btn" onClick={onRemove}>
+          <GoTrashcan size={20} />
+        </Button>
+      )}
+    </ExplorerItemCardContainer>
+  )
 }
 
 export function SoundExplorer({
   ableToPlaySound,
   ableToRemoveItem,
+  onSelect,
+  className,
 }: SoundExplorerProps) {
   const [currentSoundPath, setCurrentSoundPath] = useState('/')
   const [soundList, setSoundList] = useState<ExplorerItem[]>([])
@@ -44,7 +77,7 @@ export function SoundExplorer({
   }, [currentSoundPath])
 
   return (
-    <Container>
+    <Container className={className || ''}>
       <Header>
         {!['/', ''].includes(currentSoundPath) && (
           <Button onClick={handleBackPath}>
@@ -55,35 +88,31 @@ export function SoundExplorer({
       </Header>
       <VerticalList>
         {soundList.map(explorerItem => (
-          <Button
+          <ExplorerItemCard
             key={explorerItem.name}
-            onClick={
-              explorerItem.type === 'folder'
-                ? () => handleNavigateToFolder(explorerItem.name)
-                : ableToPlaySound
-                ? () =>
-                    playSound({
-                      path: `${currentSoundPath}/${explorerItem.name}`,
-                      name: explorerItem.name,
-                    })
-                : () => null
+            onSelect={() => {
+              const currentSound = {
+                path: `${currentSoundPath}/${explorerItem.name}`,
+                name: explorerItem.name,
+              }
+
+              if (ableToPlaySound) {
+                playSound(currentSound)
+                return
+              }
+
+              if (!onSelect) return
+
+              onSelect(currentSound)
+            }}
+            onNavigate={() => handleNavigateToFolder(explorerItem.name)}
+            onRemove={
+              ableToRemoveItem
+                ? () => handleRemoveExplorerItem(explorerItem)
+                : undefined
             }
-          >
-            {explorerItem.type === 'sound' && <MdMusicNote size={25} />}
-            {explorerItem.type === 'folder' && <MdFolder size={25} />}
-            <h2>{explorerItem.name}</h2>
-            {ableToRemoveItem && (
-              <Button
-                onClick={event => {
-                  event.stopPropagation()
-                  handleRemoveExplorerItem(explorerItem)
-                }}
-                className="delete-btn"
-              >
-                <GoTrashcan size={20} />
-              </Button>
-            )}
-          </Button>
+            {...explorerItem}
+          />
         ))}
       </VerticalList>
     </Container>
--- src/components/SoundExplorer/styles.ts
+++ src/components/SoundExplorer/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
+import { Container as ButtonContainer } from '../Button/styles'
 
 export const Container = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 0.5rem;
 
   .delete-btn {
     background: ${ctx => ctx.theme.colors.text.secondary};
-    color: ${ctx => ctx.theme.colors.box.primary};
+    color: red;
   }
 `
 
@@ -15,10 +15,35 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
 
+  min-height: 2.625rem;
+  margin-bottom: 0.5rem;
+
   h2 {
     flex: 1;
     text-align: center;
 
     color: ${ctx => ctx.theme.colors.box.primary};
+  }
+`
+
+export const ExplorerItemCardContainer = styled.div`
+  display: flex;
+  background: #5bbbbe;
+
+  border-radius: ${ctx => ctx.theme.effects.borderRadius};
+
+  ${ButtonContainer}:first-child {
+    flex: 1;
+  }
+
+  &.editable {
+    ${ButtonContainer}:first-child {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+  }
+
+  .delete-btn {
+    margin: 0.75rem;
   }
 `
